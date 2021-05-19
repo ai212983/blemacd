1118db55f4d319c39e93e5b3494f0aa060beb7e3
Dimitri Fedorov <dimitri.fedorov@suprematic.net>
2021-05-19 19:24:52 +0200

    remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,4 @@
-use std::slice::Iter;
-
 pub mod handlers;
 pub mod shutting_down_stream;
 pub mod input_token;
 pub mod byte_slices;
-
-#[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
-pub enum Position {
-    Init,
-    Root,
-    Device,
-    Service,
-    Characteristic,
-}
-
-impl Position {
-    const COUNT: usize = 5;
-
-    pub fn iter() -> Iter<'static, Position> {
-        static POSITIONS: [Position; Position::COUNT] = [
-            Position::Init,
-            Position::Root,
-            Position::Device,
-            Position::Service,
-            Position::Characteristic,
-        ];
-        POSITIONS.iter()
-    }
-
-    pub fn count() -> usize {
-        return Position::COUNT;
-    }
-}
